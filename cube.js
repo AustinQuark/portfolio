796eb04d273ed0df8805ce17c2bb429d0bd56bd1
AustinFlap <austin.vieira@live.fr>
2022-05-10 13:48:36 +0200

    cube rotate

--- cube.js
+++ cube.js
@@ -1,10 +1,21 @@
 var cube = document.getElementById("cube");
 
+//Top, Left, Right, Bot
+var swapAngles = [[1,0,0], [0, -1, 0], [0, 1, 0], [-1, 0, 0]];
+//Front, Back, Top, Bot, Left, Right
+var frontAngle = [[0, 0, 1], [0, 0, -1], [0, 1, 0], [0, -1, 0], [-1, 0, 0], [1, 0, 0]];
+
+var currentFace = [0, 0, 1]; //Front
+
 document.documentElement.addEventListener("click", function (e) {
+    var swap;
     switch (e.target.id) {
-        case "top": cube.style.transform += "rotateX(90deg)";
-        case "bot": cube.style.transform -= "rotateX(90deg)";
-        case "left": cube.style.transform -= "rotateY(90deg)";
-        case "right": cube.style.transform += "rotateY(90deg)";
+        case 'top': swap = swapAngles[0]; break;
+        case 'left': swap = swapAngles[1]; break;
+        case 'right': swap = swapAngles[2]; break;
+        case 'bot': swap = swapAngles[3]; break;
     }
+    cube.style.transform += `rotate3d(${swap[0]}, ${swap[1]}, ${swap[2]}, 90deg)`;
+
+    console.log(swap);
 })
