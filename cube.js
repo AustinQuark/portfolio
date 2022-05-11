0eaff8a4e993bb3ad24d5db80ac903ab23a9238a
AustinFlap <austin.vieira@live.fr>
2022-05-11 13:08:01 +0200

    cube rotating missing 90 + 90 each side

--- cube.js
+++ cube.js
@@ -1,22 +1,64 @@
 var cube = document.getElementById("cube");
 
-//Top, Left, Right, Bot
-var swapAngles = [[1,0,0], [0, -1, 0], [0, 1, 0], [-1, 0, 0]];
-//Front, Back, Top, Bot, Left, Right
-var frontAngle = [[0, 0, 1], [0, 0, -1], [0, 1, 0], [0, -1, 0], [-1, 0, 0], [1, 0, 0]];
-
 var currentFace = [0, 0, 1]; //Front
+
+//Front, Back, Top, Bot, Left, Right
+var strFrontAngle = ["Front", "Back", "Top", "Bot", "Left", "Right"];
+var frontAngle = [[0, 0, 1], [0, 0, -1], [0, -1, 0], [0, 1, 0], [-1, 0, 0], [1, 0, 0]];
+
+function swapTop(currentFace) {
+    var newCurrentFace = [currentFace[0], -currentFace[2], currentFace[1]];
+    return newCurrentFace;
+}
+
+function swapLeft(currentFace) {
+    var newCurrentFace = [-currentFace[2], currentFace[1], currentFace[0]];
+    return newCurrentFace
+}
+
+function swapBot(currentFace) {
+    var newCurrentFace = [currentFace[0], currentFace[2], -currentFace[1]];
+    return newCurrentFace
+}
+
+function swapRight(currentFace) {
+  var newCurrentFace = [currentFace[2], currentFace[1], -currentFace[0]];
+  return newCurrentFace
+}
+
+function arrayEquals(a, b) {
+  return Array.isArray(a) && Array.isArray(b) &&
+            a.length === b.length &&
+            a.every((val, index) => val === b[index]);
+}
 
 document.documentElement.addEventListener("click", function (e) {
     var swap;
+    var strSwap;
     switch (e.target.id) {
-        case 'top': swap = swapAngles[0]; break;
-        case 'left': swap = swapAngles[1]; break;
-        case 'right': swap = swapAngles[2]; break;
-        case 'bot': swap = swapAngles[3]; break;
+        case 'top': swap = swapTop; break;
+        case 'left': swap = swapLeft; break;
+        case 'right': swap = swapRight; break;
+        case 'bot': swap = swapBot; break;
         default: return;
     }
-    cube.style.transform += `rotate3d(${swap[0]}, ${swap[1]}, ${swap[2]}, 90deg)`;
 
-    console.log(swap);
+    currentFace = swap(currentFace);
+    for (var i = 0; i < frontAngle.length; i++)
+    {
+        if (arrayEquals(currentFace, frontAngle[i]))
+        {
+            strSwap = strFrontAngle[i];
+            break;
+        }
+    }
+
+    console.log(strFrontAngle[i], strSwap);
+    console.log("show" + strSwap)
+        
+    cube.removeAttribute("class")
+    cube.classList.add("cube")
+    cube.classList.toggle("show" + strSwap);
+    console.log(cube.classList);
+
 })
