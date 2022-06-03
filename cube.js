f949cb9c30dd58d257039bdfb1cac06e3e95dc01
AustinFlap <austin.vieira@live.fr>
2022-06-04 01:03:33 +0200

    hello

--- cube.js
+++ cube.js
@@ -18,21 +18,20 @@ var r = document.querySelector(":root");
 function temp () {};
 
 
+const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
 
-
-document.addEventListener("mousemove", function(event, radarStatus) {
+document.addEventListener("mousemove", async function(event, radarStatus) {
       vectorX = event.clientX - x0
       vectorY = event.clientY - y0
-      setTimeout(() => distance = Math.sqrt(Math.pow(vectorX, 2) + Math.pow(vectorY, 2)), 500);
+      distance = Math.sqrt(Math.pow(vectorX, 2) + Math.pow(vectorY, 2));
       if (distance < 150)
       {
         angle = Math.atan2(vectorX, vectorY)
         /*angle = 180 * angle/Math.PI;
           angle = 180 +  Math.round(angle) % 360;*/
-        console.log("allo");
 
         r.style.setProperty('--vectorX', `${vectorX}`, "important");
         r.style.setProperty('--vectorX', `${vectorX}`, "important");
         r.style.setProperty('--angle', `${angle * 10}rad`, "important");
-      }
+      };
 });
