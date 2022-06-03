e3a67fd16bf3a8c2b3203caf686ab756e2b40692
AustinFlap <austin.vieira@live.fr>
2022-06-03 20:05:14 +0200

    ...

--- cube.js
+++ cube.js
@@ -1,6 +1,15 @@
 var cube = document.getElementById("cube");
 var radar = document.getElementById("radar");
 
+var radarStatus;
+
+radar.addEventListener("mouseenter", function () {radarStatus = true;})
+radar.addEventListener("mouseout", function () {radarStatus = false;})
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
 let distance = 0;
 let rect = radar.getBoundingClientRect();
 
@@ -11,15 +20,32 @@ let vectorX;
 let vectorY;
 let angle;
 
-radar.addEventListener("mousemove", function(e) {
-    vectorX = event.clientX - x0;
-    vectorY = event.clientY - y0;
-    distance = Math.sqrt(Math.pow(vectorX, 2) + Math.pow(vectorY, 2));
-    
-    angle = Math.atan2(vectorX, vectorY);
-    angle = 180 * angle/Math.PI;
-    angle = 180 +  Math.round(angle) % 360;
-    console.log(angle);
-    console.log(Math.sin(angle) * 10.0);
-    cube.style.transform += "rotate3d(" + vectorX + ", " + vectorY + ", 0, " + angle + "deg)";
+let dirX = 0;
+let dirY = 0;
+let dirZ = 1;
+
+radar.addEventListener("mouseenter", function (e) {
+    while(radarStatus == false)
+    {
+        vectorX = e.clientX - x0
+        vectorY = e.clientY - y0
+        distance = Math.sqrt(Math.pow(vectorX, 2) + Math.pow(vectorY, 2))
+
+        angle = Math.atan2(vectorX, vectorY);
+        /*angle = 180 * angle/Math.PI;
+        angle = 180 +  Math.round(angle) % 360;*/
+
+        console.log(
+            "VectorMouse [" +
+            Math.round(vectorX) +
+            ", " +
+            Math.round(vectorY) +
+            "] "
+        );
+        console.log("Angle" + angle);
+
+        cube.style.transform +=
+            "rotate3d(" + vectorY + ", " + vectorX + ", 0, " + angle + "deg)";
+        sleep(500);
+    }
 });
