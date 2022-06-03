b5d795c646492ca48c4a3a73be28dbe031196641
AustinFlap <austin.vieira@live.fr>
2022-06-03 22:41:02 +0200

    lol

--- cube.js
+++ cube.js
@@ -1,14 +1,10 @@
 var cube = document.getElementById("cube");
 var radar = document.getElementById("radar");
 
-var radarStatus;
+var radarStatus = false;
 
 radar.addEventListener("mouseenter", function () {radarStatus = true;})
 radar.addEventListener("mouseout", function () {radarStatus = false;})
-
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms))
-}
 
 let distance = 0;
 let rect = radar.getBoundingClientRect();
@@ -24,28 +20,27 @@ let dirX = 0;
 let dirY = 0;
 let dirZ = 1;
 
-radar.addEventListener("mouseenter", function (e) {
-    while(radarStatus == false)
-    {
-        vectorX = e.clientX - x0
-        vectorY = e.clientY - y0
-        distance = Math.sqrt(Math.pow(vectorX, 2) + Math.pow(vectorY, 2))
+function temp () {}
 
-        angle = Math.atan2(vectorX, vectorY);
+var rotation = document.createElement("rotation")
+rotation.type = "text/css"
+rotation.innerHTML = ".cssClass { color: #F00; }"
+
+document.addEventListener("mousemove", function(event, radarStatus) {
+    if (radar.matches(':hover')) {
+      vectorX = event.clientX - x0
+      vectorY = event.clientY - y0
+      distance = Math.sqrt(Math.pow(vectorX, 2) + Math.pow(vectorY, 2))
+      if (distance < 300)
+      {
+        angle = Math.atan2(vectorX, vectorY)
         /*angle = 180 * angle/Math.PI;
-        angle = 180 +  Math.round(angle) % 360;*/
-
-        console.log(
-            "VectorMouse [" +
-            Math.round(vectorX) +
-            ", " +
-            Math.round(vectorY) +
-            "] "
-        );
-        console.log("Angle" + angle);
+          angle = 180 +  Math.round(angle) % 360;*/
+        console.log("Angle" + angle)
 
         cube.style.transform +=
-            "rotate3d(" + vectorY + ", " + vectorX + ", 0, " + angle + "deg)";
-        sleep(500);
+          "rotate3d(" + vectorY + ", " + vectorX + ", 0, " + angle + "deg)"
+      }
+      setTimeout(temp, 500);
     }
 });
