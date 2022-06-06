91deb17a4fb6d68919eef172ae6bb32850555fea
AustinFlap <austin.vieira@live.fr>
2022-06-06 23:08:43 +0200

    poop

--- cube.js
+++ cube.js
@@ -2,13 +2,13 @@ var radar = {
     elem: document.getElementById("radar"),
     shape: 0,
     x: 0,
-    y: 0
+    y: 0,
+    on: false
 }
 
 radar.shape = radar.elem.getBoundingClientRect();
 radar.x = radar.shape.left + radar.shape.width / 2;
-radar.y = radar.shape.top + radar.shape.height / 2
-
+radar.y = radar.shape.top + radar.shape.height / 2;
 
 var mouse = {
   x: 0,
@@ -24,15 +24,30 @@ var angle = {
 
 var speedInvert = 50;
 var root = document.querySelector(":root");
+var clock = false;
+
+var a = 0;
 
 function rotateCube() {
-	angle.x -= mouse.x / speedInvert;
-	angle.y -= mouse.y / speedInvert;
+    a += angle.x * 10;
+    a %= 360;
 
 	/*angle = 180 * angle/Math.PI;
 						angle = 180 +  Math.round(angle) % 360;*/
-	root.style.setProperty("--angleX", `${angle.y}deg`, "important");
+	root.style.setProperty("--angleX", `${a}deg`, "important");
 	root.style.setProperty("--angleY", `${angle.x}deg`, "important");
+}
+
+function decrementSpeed() {
+    if (Math.abs(angle.x) > 0 || Math.abs(angle.y) > 0)
+    {
+        angle.x -= Math.sign(angle.x) * 0.05; 
+        angle.y -= Math.sign(angle.y) * 0.05;
+        rotateCube();
+        return ;
+    }
+    clearInterval(clock);
+    clock = false;
 }
 
 document.addEventListener("mousemove", function(e) { 
@@ -40,6 +55,23 @@ document.addEventListener("mousemove", function(e) {
     mouse.y = e.clientY - radar.y;
 
     distance = Math.sqrt(Math.pow(mouse.x, 2) + Math.pow(mouse.y, 2));
+	angle.x = mouse.x / distance;
+    angle.y = mouse.y / distance;
+    console.log(angle.x, angle.y);
 
-    rotateCube();
+    if (distance <= radar.shape.width / 2)
+    {
+        if (!clock)
+            clock = setInterval(rotateCube, 15);
+        radar.on = true;
+    }
+    else
+    {
+        radar.on = false;
+        clearInterval(clock);
+        clock = false;
+        //clock = setInterval(decrementSpeed, 15);
+        
+    }
+
 });
