93597da49eef5c7c3e227bd9d15f55f944cdb2dc
AustinFlap <austin.vieira@live.fr>
2022-06-08 19:59:23 +0200

    console.log rm

--- description.js
+++ description.js
@@ -4,11 +4,9 @@ var nameplate = document.getElementsByClassName("nameplate-letter");
 
 for (var i = 0; i < nameplate.length; i++) {
     nameplate[i].addEventListener("mouseover", function(e) {
-        console.log("over");
         description.style.transform = "translateY(15px)";
     })
     nameplate[i].addEventListener("mouseout", function (e) {
-        console.log("over")
         description.style.transform = ""
     })
 }
