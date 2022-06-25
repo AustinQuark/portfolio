2dc49ddb2b43c5ca5be5f738b839b57e3dbcff68
AustinFlap <austin.vieira@live.fr>
2022-06-25 14:59:56 +0200

    rework, threejs from cdn

--- cubeThree.js
+++ cubeThree.js
@@ -1,9 +1,13 @@
+import * as THREE from "three";
+import { OrbitControls } from "https://unpkg.com/three/examples/jsm/controls/OrbitControls.js";
+
+
 const scene = new THREE.Scene();
 const cubeContainer = document.getElementById("cube_container");
 const camera = new THREE.PerspectiveCamera(75, cubeContainer.offsetWidth / cubeContainer.offsetHeight);
 
 const renderer = new THREE.WebGLRenderer({ alpha: false });
-renderer.setSize(cubeContainer.offsetWidth, cubeContainer.offsetHeight)
+renderer.setSize(cubeContainer.offsetWidth, cubeContainer.offsetHeight);
 renderer.domElement.style.borderRadius = "50%";
 cubeContainer.appendChild(renderer.domElement);
 
@@ -13,15 +17,14 @@ const cube = new THREE.Mesh(geometry, material);
 
 scene.add(cube);
 
-var control = new THREE.OrbitControls(camera, renderer.domElement);
+var control = new OrbitControls(camera, renderer.domElement);
 control.enableDamping = true;
 
 camera.position.z = 2;
 
-
 function animate() {
 	requestAnimationFrame(animate);
-    control.update();
+	control.update();
 	renderer.render(scene, camera);
 }
 
