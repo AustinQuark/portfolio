bf5b87aac23d36846a1390abd20b5056254c7053
AustinFlap <austin.vieira@live.fr>
2022-07-01 08:58:40 +0200

    swipe cube on mobile, colors

--- cubeThree.js
+++ cubeThree.js
@@ -17,8 +17,6 @@ scene.background = new THREE.Color(0x414141);
 const camera = new THREE.PerspectiveCamera(70, cubeContainer.offsetWidth / cubeContainer.offsetHeight);
 camera.position.x = cubeSize * 1.7;
 
-
-
 const faceRenderer = new CSS3DRenderer({antialias: true});
 faceRenderer.setSize(cubeContainer.offsetWidth, cubeContainer.offsetHeight);
 faceRenderer.domElement.style.borderRadius = "50%";
@@ -32,29 +30,80 @@ const geometry = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
 const material = new THREE.MeshBasicMaterial({ color: 0x520900 });
 const cube = new THREE.Mesh(geometry, material);
 
-function is_touch_enabled() {
-	return "ontouchstart" in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
-}
+var isTouch =  "ontouchstart" in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
+
+//Desktop followed by Mobile camera gestion.
 
 var control = new OrbitControls(camera, faceRenderer.domElement);
 control.enableDamping = true;
 control.enableZoom = false;
 control.enablePan = false;
-//
 
-if (is_touch_enabled()) {
+if (isTouch) {
     control.enabled = false;
-    var transformControls = new TransformControls(camera, faceRenderer.domElement);
-    transformControls.attach(cube);
-    transformControls.setMode("rotate");
 
-    scene.add(transformControls);
+	// Source : https://stackoverflow.com/questions/2264072/detect-a-finger-swipe-through-javascript-on-the-iphone-and-android
+	document.addEventListener("touchstart", handleTouchStart, false);
+	document.addEventListener("touchmove", handleTouchMove, false);
+}
+
+var xDown = null;
+var yDown = null;
+
+function getTouches(evt) {
+    return (
+        evt.touches || // browser API
+        evt.originalEvent.touches
+    ); // jQuery
+}
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+}
+
+function handleTouchMove(evt) {
+    if (!xDown || !yDown) {
+        return;
+    }
+    var xSwipe = 0;
+    var ySwipe = 0;
+
+    var xUp = evt.touches[0].clientX;
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {
+        /*most significant*/
+        if (xDiff > 0) {
+            xSwipe = Math.PI / -2;
+        } else {
+            xSwipe = Math.PI / 2;
+        }
+    } else {
+        if (yDiff > 0) {
+            ySwipe = Math.PI / 2;
+        } else {
+            ySwipe = Math.PI / -2;
+        }
+    }
+
+    var deltaRotationQuaternion = new THREE.Quaternion().setFromEuler(new THREE.Euler(0, xSwipe, ySwipe, "XYZ"));
+    deltaRotationQuaternion.multiply(cube.quaternion);
+    var rotation = new THREE.Euler().setFromQuaternion(deltaRotationQuaternion, 'XYZ');
+
+    new TWEEN.Tween(cube.rotation).to({ y: rotation.y, z: rotation.z }, 1500).easing(TWEEN.Easing.Back.InOut).start();
+
+    /* reset values */
+    xDown = null;
+    yDown = null;
 }
 
 cubeContainer.appendChild(faceRenderer.domElement);
 cubeContainer.appendChild(renderer.domElement);
-
-
 
 var cubeFile= new XMLHttpRequest();
 cubeFile.open("GET", './cubeContent.json', true);
@@ -80,6 +129,10 @@ cubeFile.onreadystatechange = function ()
             label.rotation.x = 0;
             label.rotation.y = (i == 1) ? 0 : (i == 2) ? -Math.PI / 2 : (i == 0) ? Math.PI / 2 : Math.PI;
             label.rotation.z = 0;
+
+            if (isTouch)
+                if (i == 2)
+                    label.rotation.x = Math.PI / 2;
         }
         else
         {
@@ -102,11 +155,10 @@ cubeFile.send(null);
 
 scene.add(cube);
 
-
 var halfTime = 1000;
 const bounce = () => {
 	new TWEEN.Tween(cube.position)
-		.to({ y: 3 }, halfTime)
+		.to({ y: 5 }, halfTime)
 		.easing(TWEEN.Easing.Quadratic.InOut)
 		.start()
 		.onComplete(() => {
@@ -115,6 +167,8 @@ const bounce = () => {
 };
 bounce();
 setInterval(bounce, halfTime * 2);
+
+
 
 function animate() {
 	requestAnimationFrame(animate);
