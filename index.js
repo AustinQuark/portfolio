17c737a64109d4bd9b12eb182da726086152c6ad
Austin <austin.vieira@live.fr>
2023-03-06 19:20:58 +0100

    rework

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import captureWebsite from 'capture-website';
+import cron from 'node-cron';
+
 
 const __filename = fileURLToPath(import.meta.url);
 
@@ -21,3 +24,36 @@ app.use('/', router);
 app.listen(PORT, () => {
     console.log('Server Listened');
 })
+
+cron.schedule('0 0 * * *', () => {
+    captureWebsite.file('', 'screenshot', {
+    });
+});
+
+const options = {
+    launchOptions: {
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox'
+        ]
+    },
+    delay: 5,
+    width: 854,
+    height: 480,
+    type: 'webp',
+    darkMode: true,
+    overwrite: true
+};
+
+const items = [
+	['https://projectbar-events.com/index', 'public/images/screenshot0'],
+	['https://github.com/AustinQuark/philosophers', 'public/images/screenshot1'],
+    ['https://github.com/AustinQuark/push_swap', 'public/images/screenshot2'],
+    ['https://github.com/AustinQuark/VanillaStopWatch', 'public/images/screenshot3'],
+    ['https://github.com/AustinQuark/cub3D', 'public/images/screenshot4'],
+    ['https://github.com/AustinQuark/minishell_42', 'public/images/screenshot5']
+];
+
+await Promise.all(items.map(([url, filename]) => {
+	return captureWebsite.file(url, `${filename}.png`, options);
+}));
---
+++ public/scripts/cubeThreeLegacyBis.js
@@ -0,0 +1,225 @@
+// Skybox's Author : Hannes Delbeke
+// Source : https://sketchfab.com/3d-models/fantasy-sky-background-15c79bb2fc1147128039fe4ff90fd5a0
+import * as THREE from "./three.module.js";
+import CameraControls from "./camera-controls.module.js";
+import { CSS3DRenderer, CSS3DObject  } from "./CSS3DRenderer.js";
+import { TWEEN } from "./tween.module.min.js";
+
+var cubeSize = 400;
+
+const cubeContainer = document.getElementById("cube_container");
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color("rgb(166, 251, 255)");
+
+const camera = new THREE.PerspectiveCamera(65, cubeContainer.offsetWidth / cubeContainer.offsetHeight, 1, 2000);
+camera.position.x = cubeSize * 1.7;
+
+//CSS Renderer
+const faceRenderer = new CSS3DRenderer({antialias: true});
+faceRenderer.domElement.style.borderRadius = "50%";
+faceRenderer.domElement.style.position = "absolute";
+
+
+//Standart Renderer
+const renderer = new THREE.WebGLRenderer({ antialias: true , alpha: true});
+renderer.domElement.style.borderRadius = "50%";
+renderer.setPixelRatio(window.devicePixelRatio);
+
+
+//Cube Mesh
+const geometry = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
+const material = new THREE.MeshBasicMaterial({opacity:0, transparent:true});
+const cube = new THREE.Mesh(geometry, material);
+cube.scale.set(0,0,0);
+
+//Skybox Mesh
+const skyboxGeometry = new THREE.SphereGeometry(800, 60, 40);
+skyboxGeometry.scale(-1, 1, 1);
+const skyboxTexture = new THREE.TextureLoader().load("../images/skybox.jpg");
+const skyboxMaterial = new THREE.MeshBasicMaterial({ map: skyboxTexture, opacity:0, transparent:true });
+const skybox = new THREE.Mesh(skyboxGeometry, skyboxMaterial);
+
+//Orbit Control for Desktop
+CameraControls.install({ THREE: THREE });
+var control = new CameraControls(camera, faceRenderer.domElement);
+control.enableDamping = true;
+control.dollySpeed = 0;
+control.truckSpeed = 0;
+
+control.enablePan = false;
+control.azimuthRotateSpeed = 0.4;
+control.polarRotateSpeed = 0.4;
+
+cubeContainer.appendChild(faceRenderer.domElement);
+cubeContainer.appendChild(renderer.domElement);
+
+function readTextFile(file, callback) {
+	var rawFile = new XMLHttpRequest();
+	rawFile.overrideMimeType("application/json");
+	rawFile.open("GET", file, true);
+	rawFile.onreadystatechange = function () {
+		if (rawFile.readyState === 4 && rawFile.status == "200") {
+			callback(rawFile.responseText);
+		}
+	};
+	rawFile.send(null);
+}
+
+readTextFile("scripts/cubeContent.json", function (text) {
+	var cubeContent = JSON.parse(text);
+	for (var i = 0; i < cubeContent.length; i++) {
+		var faceElem = document.createElement("face");
+		faceElem.className = "face";
+		faceElem.style.width = cubeSize + "px";
+		faceElem.style.height = cubeSize + "px";
+
+
+		var label = new CSS3DObject(faceElem);
+		if (i < 4) {
+			label.position.x = i % 2 ? 0 : i ? cubeSize / -2 : cubeSize / 2;
+			label.position.y = 0;
+			label.position.z = !(i % 2) ? 0 : i - 1 ? cubeSize / -2 : cubeSize / 2;
+
+			label.rotation.x = 0;
+			label.rotation.y = i == 1 ? 0 : i == 2 ? -Math.PI / 2 : i == 0 ? Math.PI / 2 : Math.PI;
+			label.rotation.z = 0;
+		} else {
+			label.position.x = 0;
+			label.position.y = i % 2 ? cubeSize / -2 : cubeSize / 2;
+			label.position.z = 0;
+
+			label.rotation.x = -Math.PI / 2;
+			label.rotation.y = i == 5 ? Math.PI : 0;
+			label.rotation.z = Math.PI / 2;
+		}
+		var face = document.createElement("img");
+		face.style.width = "100%";
+		face.style.height = "100%";
+		face.src = `images/screenshot${i}.png`;
+		face.style.imageRendering = "crisp-edges";
+		face.style.zIndex = "1";
+		var link = document.createElement("a");
+		link.classList.add("linkCube");
+		link.href = "#";
+		var object = document.createElement("object");
+		object.style = "width:75%; height:75%; top:50%; left:50%; transform:translate(-50%, -50%); position:absolute;filter: invert(17%) sepia(14%) saturate(1039%) hue-rotate(144deg) brightness(91%) contrast(87%);";
+		object.data = "images/link-external.svg";
+		object.type = "image/svg+xml";
+		object.style.pointerEvents = "none";
+
+		link.appendChild(object);
+		faceElem.appendChild(link);
+		faceElem.appendChild(face);
+		cube.add(label);
+/*
+        header.addEventListener("mouseenter", function (e) {
+            if (!control.currentAction) control.enabled = false;
+        });
+
+        header.addEventListener("mouseleave", function (e) {
+            control.enabled = true;
+        });*/
+	}
+});
+
+document.addEventListener("fileLoaded", function () {
+	var links = document.getElementsByTagName("a");
+	for (var i = 0; i < links.length; i++) {
+		links[i].addEventListener("mousedown", function () {
+			window.open(this.getAttribute("href"));
+		});
+	}
+});
+
+scene.add(cube);
+scene.add(skybox);
+
+//Bounce effect
+var halfTime = 1000;
+const bounce = () => {
+	new TWEEN.Tween(cube.position)
+		.to({ y: 5 }, halfTime)
+		.easing(TWEEN.Easing.Quadratic.InOut)
+		.start()
+		.onComplete(() => {
+			new TWEEN.Tween(cube.position).to({ y: 0 }, halfTime).easing(TWEEN.Easing.Quadratic.InOut).start();
+		});
+};
+bounce();
+setInterval(bounce, halfTime * 2);
+
+//Resizer (source : https://discoverthreejs.com/book/first-steps/responsive-design/)
+const setSize = (container, camera, renderer, faceRenderer) => {
+	camera.aspect = container.clientWidth / container.clientHeight;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize(container.clientWidth, container.clientHeight);
+	renderer.setPixelRatio(window.devicePixelRatio);
+    faceRenderer.setSize(container.clientWidth, container.clientHeight);
+};
+
+class Resizer {
+	constructor(container, camera, renderer, faceRenderer) {
+		// set initial size
+		setSize(container, camera, renderer, faceRenderer);
+
+		window.addEventListener("resize", () => {
+			// set the size again if a resize occurs
+			setSize(container, camera, renderer, faceRenderer);
+		});
+	}
+}
+
+const clock = new THREE.Clock();
+
+function animate() {
+    const delta = clock.getDelta();
+    control.update(delta);
+	TWEEN.update();
+	new Resizer(cubeContainer, camera, renderer, faceRenderer);
+	faceRenderer.render(scene, camera);
+	renderer.render(scene, camera);
+	requestAnimationFrame(animate);
+}
+
+cubeContainer.classList.add("cube_pop");
+new TWEEN.Tween(skyboxMaterial)
+    .to({ opacity: 1 }, 3000)
+    .easing(TWEEN.Easing.Cubic.Out)
+    .start();
+new TWEEN.Tween(cube.scale)
+    .to({ x: 1, y: 1, z: 1 }, 3000)
+    .easing(TWEEN.Easing.Cubic.Out)
+    .start()
+    .onComplete(() => {cubeContainer.style.transitionDuration = "0s";})
+new TWEEN.Tween(cube.rotation)
+	.to({ x: 0, y: 6 * Math.PI, z: 4 * Math.PI }, 3000)
+	.easing(TWEEN.Easing.Back.Out)
+	.start()
+	.onComplete(() => {
+        document.dispatchEvent(new CustomEvent("fileLoaded"));
+	});
+
+
+control.addEventListener("controlstart", function(e){
+        new TWEEN.Tween(camera)
+            .to({ fov: 90 }, 500)
+            .easing(TWEEN.Easing.Cubic.Out)
+            .onUpdate(function (camera) {
+                camera.updateProjectionMatrix();
+            })
+            .start();
+});
+
+control.addEventListener("controlend", function (e) {
+	new TWEEN.Tween(camera)
+		.to({ fov: 65 }, 500)
+		.easing(TWEEN.Easing.Cubic.Out)
+		.onUpdate(function (camera) {
+			camera.updateProjectionMatrix();
+		})
+		.start();
+});
+
+animate();
